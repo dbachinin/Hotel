945952f3e2b8f5eaa99618ee9724dd060fc14c7b
tg-it-03 <dbachinin@gmail.com>
2017-06-19 13:24:38 +0300

    working booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,24 +1,37 @@
 class Booking < ApplicationRecord
 	def calc_subtotal(room)
-		days_count = (self.check_in - self.check_out).to_i
-		tar = room.price.last.taryph
-		@cen = []
-		@tar = []; tar.each {|i| @tar.push(i.id) if self.check_in > i.udate and i.edate > self.check_in or i.udate == self.check_in }
-		@tar.each do |i|
-			cen = @room.price.last.price + tar.find(i).index; p cen
-			temp = (check_in - tar.find(i).edate).to_i.abs
-			@cen.push(temp) if Date.parse(check_in.to_s) > Date.parse(tar.find(i).udate.to_s) and Date.parse(check_out.to_s) > Date.parse(tar.find(i).edate.to_s); temp = nil
-			temp = (tar.find(i).udate - tar.find(i).edate).to_i.abs
-			@cen.push(temp) if Date.parse(check_in.to_s) < Date.parse(tar.find(i).udate.to_s) and Date.parse(check_out.to_s) > Date.parse(tar.find(i).edate.to_s); temp = nil
-			temp = (tar.find(i).edate - check_out).to_i.abs
-			@cen.push(temp) if Date.parse(check_out.to_s) > Date.parse(tar.find(i).edate.to_s); temp = nil
-			temp = (check_in - check_out).to_i.abs
-			@cen.push(temp) if Date.parse(check_out.to_s) < Date.parse(tar.find(i).edate.to_s) and Date.parse(check_in.to_s) < Date.parse(tar.find(i).udate.to_s); temp = nil
+		# days_count = (self.check_in - self.check_out).to_i
 
+		tar = Room.find(room).price.last.taryph
+		@cena ||=0
+		zaezd = Date.parse(self.check_in.to_s)
+		otezd = Date.parse(self.check_out.to_s)
+		 
+		@tar = []; tar.each do |i| 
+			nachalo_perioda = Date.parse(i.udate.to_s)
+			conec_perioda = Date.parse(i.edate.to_s)
+			@tar.push(i.id) if zaezd >= nachalo_perioda and zaezd < conec_perioda
+			@tar.push(i.id) if nachalo_perioda > zaezd and nachalo_perioda < otezd
+		end
+		@tar.each do |i|
+			cena = Room.find(room).price.last.price + tar.find(i).index; p cena
+			nachalo_perioda = Date.parse(tar.find(i).udate.to_s)
+			conec_perioda = Date.parse(tar.find(i).edate.to_s)
+			
+			#Calculate days count for end of start price period
+			
+			length_period = (conec_perioda - zaezd).to_i           if zaezd >= nachalo_perioda and otezd >= conec_perioda; p "ppp #{length_period}"
+			length_period = (conec_perioda - nachalo_perioda).to_i if zaezd <= nachalo_perioda and otezd >= conec_perioda; p "fff #{length_period}"
+			length_period = (otezd - nachalo_perioda).to_i         if zaezd <= nachalo_perioda and otezd <= conec_perioda; p "aaa #{length_period}"
+			length_period = (otezd - zaezd).to_i                   if zaezd >= nachalo_perioda and otezd <= conec_perioda; p "ggg #{length_period}"
+			@cena = @cena + (cena * length_period.to_f)
+
+			puts @cena 
+			puts @tar 
 		end
 
 
-		
+		end
 		# price = Price.find(tar.find(@tar).price_id)
 		# cen = price + Taryph.find(@tar).index if (check_in - Taryph.find(@tar).edate).to_i > (check_in - check_out).to_i
 
