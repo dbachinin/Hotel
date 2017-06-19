5a67fb3239a62ed3e4a983b421e219222dd44f47
tg-it-03 <dbachinin@gmail.com>
2017-06-19 14:53:56 +0300

    working booking add search_employ method

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,24 +14,32 @@ class Booking < ApplicationRecord
 			@tar.push(i.id) if nachalo_perioda > zaezd and nachalo_perioda < otezd
 		end
 		@tar.each do |i|
-			cena = Room.find(room).price.last.price + tar.find(i).index; p cena
+			cena = Room.find(room).price.last.price + tar.find(i).index
 			nachalo_perioda = Date.parse(tar.find(i).udate.to_s)
 			conec_perioda = Date.parse(tar.find(i).edate.to_s)
 			
 			#Calculate days count for end of start price period
 			
-			length_period = (conec_perioda - zaezd).to_i           if zaezd >= nachalo_perioda and otezd >= conec_perioda; p "ppp #{length_period}"
-			length_period = (conec_perioda - nachalo_perioda).to_i if zaezd <= nachalo_perioda and otezd >= conec_perioda; p "fff #{length_period}"
-			length_period = (otezd - nachalo_perioda).to_i         if zaezd <= nachalo_perioda and otezd <= conec_perioda; p "aaa #{length_period}"
-			length_period = (otezd - zaezd).to_i                   if zaezd >= nachalo_perioda and otezd <= conec_perioda; p "ggg #{length_period}"
+			length_period = (conec_perioda - zaezd).to_i           if zaezd >= nachalo_perioda and otezd >= conec_perioda#; p "ppp #{length_period}"
+			length_period = (conec_perioda - nachalo_perioda).to_i if zaezd <= nachalo_perioda and otezd >= conec_perioda#; p "fff #{length_period}"
+			length_period = (otezd - nachalo_perioda).to_i         if zaezd <= nachalo_perioda and otezd <= conec_perioda#; p "aaa #{length_period}"
+			length_period = (otezd - zaezd).to_i                   if zaezd >= nachalo_perioda and otezd <= conec_perioda#; p "ggg #{length_period}"
 			@cena = @cena + (cena * length_period.to_f)
 
-			puts @cena 
-			puts @tar 
+			return @cena
+			#puts @cena 
+			#puts @tar 
 		end
 
 
 		end
+		
+		def search_employ
+			@lock = []
+			Booking.all.each {|i| @lock.push(i.id) if (Date.parse(self.check_in.to_s) - Date.parse(i.check_out.to_s)) * (Date.parse(i.check_in.to_s) - Date.parse(self.check_out.to_s)) >= 0 && i.room_id == self.room_id } 
+		    return @lock
+		end
+
 		# price = Price.find(tar.find(@tar).price_id)
 		# cen = price + Taryph.find(@tar).index if (check_in - Taryph.find(@tar).edate).to_i > (check_in - check_out).to_i
 
