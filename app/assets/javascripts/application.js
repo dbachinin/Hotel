e2728e7044523ba2ab51fe7793e17553459b4c98
tg-it-03 <dbachinin@gmail.com>
2017-07-07 18:52:36 +0300

    working a lot functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,18 @@
 //= require turbolinks
 //= require_tree .
 $('.carousel').on('mouseenter',function(){ $( this ).carousel();})
+
+$(document).ready(function(e) {
+    $('#menu_icon').click(function(){
+        if($("#content_details").hasClass('drop_menu'))
+		{
+        $("#content_details").addClass('drop_menu1').removeClass('drop_menu');
+    }
+		else{
+			$("#content_details").addClass('drop_menu').removeClass('drop_menu1');
+			}
+	
+	
+	});
+	
+});
