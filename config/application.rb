ce3b98190d94524e1950a307a5fd7e2d33e66dbb
dbachinin <dbachinin@ya.ru>
2017-07-09 19:36:22 +0300

    add locale

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module HotelLudi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.available_locales = [:ru, :en]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
