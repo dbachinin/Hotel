d76c76d2ba4735c5242767db224ab48167c0cb16
tg-it-03 <dbachinin@gmail.com>
2017-07-10 09:37:48 +0300

    I18n works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-    before_action :set_locale, :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_locale
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def extract_locale
-    parsed_locale = request.subdomains.first
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-    end
-    
-  protected
+    # def extract_locale
+    # parsed_locale = request.subdomains.first
+    # I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    # end
 
-    
+    #request.original_url.split('.').first.split('//').last
     def set_locale
-      I18n.locale = extract_locale || I18n.default_locale
+      I18n.locale = extract_locale || session[:locale] || I18n.default_locale
+      session[:locale] = I18n.locale
+
+    end
+    def extract_locale
+      parsed_locale = request.original_url.split('.').first.split('//').last || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/)[0]
+      I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
     end
 
+  protected
+  
   def configure_permitted_parameters
     added_attrs = [:login, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
